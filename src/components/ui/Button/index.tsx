fb297cae146f3c556822967719320eabfa8a5024
akashnegi62 <akashsing215@gmail.com>
2026-09-11 22:27:28 +0530

    changes

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -13,7 +13,7 @@ export const buttonVariants = cva(
         outline: 'border border-gray-300 bg-transparent hover:bg-gray-100 text-gray-900 dark:border-gray-700 dark:text-gray-100 dark:hover:bg-gray-800',
         secondary: 'bg-gray-200 text-gray-900 hover:bg-gray-300 dark:bg-gray-800 dark:text-gray-100 dark:hover:bg-gray-700',
         ghost: 'hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-100',
-        link: 'text-blue-600 underline-offset-4 hover:underline dark:text-blue-400',
+        link: 'text-blue-500 underline-offset-4 hover:underline dark:text-blue-400',
       },
       size: {
         default: 'h-10 px-4 py-2',
